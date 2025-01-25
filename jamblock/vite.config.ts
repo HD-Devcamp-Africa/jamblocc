25ebf89d155232c97aeddef5eb434a9a355cdd8b
Moses-main <moseschizaram8@gmail.com>
2025-01-25 03:08:31 +0100

    configure the vite.config.ts to handle manual navigations

--- jamblock/vite.config.ts
+++ jamblock/vite.config.ts
@@ -1,7 +1,27 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  server: {
+    host: true, // Optional: Expose the server to external devices
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Optimize chunking if needed
+      },
+    },
+  }
+}); 
